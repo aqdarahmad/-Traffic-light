60ff8de241656a97519247592c3f88eda7e89e12
Aqdar Ahmad <a.abusufyan@student.aaup.edu>
2025-08-26 07:39:45 +0300

    Update test.sv

--- test.sv
+++ test.sv
@@ -1,39 +1,19 @@
-reg clock,reset;
-reg x_in;
-wire y_out;
-Moore_Model um(y_out,x_in,clock,reset);
- initial
- $monitor($time," reset=%b , x_in=%b , y_out=%b ",reset,x_in,y_out);
- initial
- begin
- clock = 0;
- forever #25 clock = ~clock;
- end
- initial
- begin
- reset = 0;
- #5 reset=1;
- //010111110111100
-#5x_in=0;
- #5x_in=1;
- #5x_in=0;
- #5x_in=1;
- #5x_in=1;
- #5x_in=1;
- #5x_in=1;
- #5 x_in=1;
- #5 x_in=0;
- #5x_in=1;
- #5x_in=1;
- #5x_in=1;
- #5x_in=1; #5x_in=0;
- //0100111
- #5reset=1;
- #10x_in=0;
- #10x_in=1; #10x_in=0;
- #10x_in=0; #10x_in=1;
- #10x_in=1; #10x_in=1;
+module test;
 
- end
- initial
- #400 $finish;
+reg redlight,oralight,grelight;
+reg clk;
+
+Trafficlight t(clk,redlight,oralight,grelight );
+initial
+monitor($time," redlight=%b , $
+oralight=%b, grelight=%b
+",redlight,oralight,grelight);
+
+initial
+begin
+clk = 0;
+forever #1 clk = ~clk;
+end
+initial
+finish;$60#
+endmodule
